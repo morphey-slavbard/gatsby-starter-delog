04065a5fb2da1e05d922bb9c72764f096f505eb4
morphey-slavbard <81485841+morphey-slavbard@users.noreply.github.com>
2022-03-04 18:40:56 +0000

    Update Hero Header

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -15,8 +15,9 @@ export default () => (
       }
     `}
     render={data => (
-      <div class="heroimage">
+      
       <div className="hero-header">
+        <div class="heroimage">
         <div className="headline">{data.site.siteMetadata.home.title}</div>
         <div 
           className="primary-content" 
