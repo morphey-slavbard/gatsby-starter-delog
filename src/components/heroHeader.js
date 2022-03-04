a8dbad1fa9966a2492615270d2173595afe1495b
morphey-slavbard <81485841+morphey-slavbard@users.noreply.github.com>
2022-03-04 18:38:12 +0000

    Added hero Image

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -15,12 +15,16 @@ export default () => (
       }
     `}
     render={data => (
+      <div class="heroimage">
       <div className="hero-header">
         <div className="headline">{data.site.siteMetadata.home.title}</div>
         <div 
           className="primary-content" 
-          dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}/>
+          dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}
+        />
         <Link to='/contact' className="button -primary">Get in touch &rarr;</Link>
+
+        </div> 
       </div>
     )}
   />
